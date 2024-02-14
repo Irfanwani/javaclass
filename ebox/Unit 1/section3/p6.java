59489c0cf0649d80214107932fe7523b957ca664
Irfanwani <irfanwani347@gmail.com>
2024-02-14 15:27:16 +0530

    added p6 and p7

--- ebox/Unit 1/section3/p6.java
+++ ebox/Unit 1/section3/p6.java
@@ -1,5 +1,34 @@
-package ebox.Unit 1.section3;
+import java.util.Scanner;
 
-public class p6 {
-    
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int subject1 = scanner.nextInt();
+
+    int subject2 = scanner.nextInt();
+
+    int subject3 = scanner.nextInt();
+
+    int totalMarks = subject1 + subject2 + subject3;
+    double averageMark = totalMarks / 3.0;
+
+    String grade;
+    if (averageMark >= 90) {
+      grade = "A";
+    } else if (averageMark >= 80) {
+      grade = "B";
+    } else if (averageMark >= 70) {
+      grade = "C";
+    } else if (averageMark >= 60) {
+      grade = "D";
+    } else if (averageMark >= 50) {
+      grade = "E";
+    } else {
+      grade = "F";
+    }
+
+    System.out.println("The grade is " + grade);
+  }
 }
+
--- ebox/Unit 1/section3/p7.java
+++ ebox/Unit 1/section3/p7.java
@@ -1,5 +1,28 @@
-package ebox.Unit 1.section3;
+import java.util.Scanner;
 
-public class p7 {
-    
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Richter Magnitude: ");
+    double magnitude = scanner.nextDouble();
+
+    String intensity;
+    if (magnitude <= 2.4) {
+      intensity = "Micro";
+    } else if (magnitude <= 5.4) {
+      intensity = "Light";
+    } else if (magnitude <= 6.0) {
+      intensity = "Moderate";
+    } else if (magnitude <= 6.9) {
+      intensity = "Strong";
+    } else if (magnitude <= 7.9) {
+      intensity = "Major";
+    } else {
+      intensity = "Great";
+    }
+
+    System.out.println(intensity);
+  }
 }
