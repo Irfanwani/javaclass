ec416377954e2f233266682e1cf75b7d723c29cb
Irfanwani <irfanwani347@gmail.com>
2024-04-02 10:30:36 +0530

    added p3, completed unit 4

--- ebox/Unit 4/section3/p3.java
+++ ebox/Unit 4/section3/p3.java
@@ -1,44 +1,43 @@
-// needs updation
-
 import java.util.*;
+import java.text.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of dates");
-        int n = scanner.nextInt();
-        scanner.nextLine(); 
+        int n = sc.nextInt();
+        sc.nextLine(); 
 
-
+        String[] dates = new String[n];
         System.out.println("Enter dates of the events:(dd/MM/yyyy)");
-        Map<String, Integer> dayCount = new HashMap<>();
-        String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
         for (int i = 0; i < n; i++) {
-            String date = scanner.nextLine();
-            Calendar calendar = Calendar.getInstance();
-            String[] parts = date.split("/");
-            int day = Integer.parseInt(parts[0]);
-            int month = Integer.parseInt(parts[1]) - 1; 
-            int year = Integer.parseInt(parts[2]);
-            calendar.set(year, month, day);
-            int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK); 
-            String dayName = daysOfWeek[dayOfWeek - 1];
-            dayCount.put(dayName, dayCount.getOrDefault(dayName, 0) + 1);
+            dates[i] = sc.nextLine();
         }
 
-
-        int maxEvents = 0;
-        String busiestDay = "";
-        for (Map.Entry<String, Integer> entry : dayCount.entrySet()) {
-            if (entry.getValue() > maxEvents || (entry.getValue() == maxEvents && entry.getKey().compareTo(busiestDay) < 0)) {
-                busiestDay = entry.getKey();
-                maxEvents = entry.getValue();
+        int[] daysCount = new int[7]; 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        for (String date : dates) {
+            try {
+                Date eventDate = sdf.parse(date);
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(eventDate);
+                int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK); 
+                daysCount[dayOfWeek - 1]++; 
+            } catch (ParseException e) {
+                e.printStackTrace();
             }
         }
 
+        int maxCount = 0;
+        int maxDayIndex = 0;
+        for (int i = 0; i < 7; i++) {
+            if (daysCount[i] > maxCount) {
+                maxCount = daysCount[i];
+                maxDayIndex = i;
+            }
+        }
 
-        System.out.println("More number of events takes place on " + busiestDay);
+        String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        System.out.println("More number of events takes place on " + daysOfWeek[maxDayIndex]);
     }
 }
